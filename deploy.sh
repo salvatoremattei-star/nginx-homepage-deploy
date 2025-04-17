d021045b3588701fd6a7a3bb1b30a71305500754
root <salvatore.mattei@digimetrica.com>
2025-04-17 11:47:27 +0200

    test8

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 set -e
 
-# Sostituisci con il tuo ID progetto
+# Impostazioni progetto
 PROJECT_ID=playground-s-11-0a723cfb
 REGION=us-central1
 ZONE=us-central1-a
 SUBNET=custom-subnet
 
-# Legge l'HTML e lo formatta per YAML
-HTML=$(cat html/index.html  | awk '{print "        value: \""$0"\""}')
+# Script di avvio (startup-script) da inserire nel template
+STARTUP_SCRIPT=$(cat <<'EOT'
+        value: |
+          #!/bin/bash
+          apt-get update
+          apt-get install -y nginx
+          cat <<EOF > /var/www/html/index.html
+          <!DOCTYPE html>
+          <html>
+          <head><title>Homepage</title></head>
+          <body>
+            <h1>Benvenuto sulla homepage aggiornata!</h1>
+          </body>
+          </html>
+EOF
+          systemctl enable nginx
+          systemctl restart nginx
+EOT
+)
 
+# Nome univoco per il nuovo template
 TEMPLATE_NAME=homepage-template-$(date +%s)
 
+# Creazione file YAML del nuovo instance template
 cat <<EOF > new-template.yaml
 resources:
 - name: $TEMPLATE_NAME
@@ -24,7 +43,7 @@ resources:
       metadata:
         items:
         - key: startup-script
-$HTML
+$STARTUP_SCRIPT
       networkInterfaces:
       - subnetwork: projects/$PROJECT_ID/regions/$REGION/subnetworks/$SUBNET
         accessConfigs:
@@ -42,13 +61,20 @@ $HTML
         scopes: [https://www.googleapis.com/auth/cloud-platform]
 EOF
 
-# Crea il nuovo instance template
+# Deploy del nuovo template
 gcloud deployment-manager deployments create $TEMPLATE_NAME --config=new-template.yaml
 
-# Ottiene il selfLink del nuovo template
-TEMPLATE=$(gcloud compute instance-templates list --filter="name=$TEMPLATE_NAME" --format="value(selfLink)" | head -n 1)
+# Recupero del selfLink del template appena creato
+TEMPLATE=$(gcloud compute instance-templates list \
+  --filter="name=$TEMPLATE_NAME" \
+  --format="value(selfLink)" | head -n 1)
 
 # Aggiorna il MIG
-gcloud compute instance-groups managed set-instance-template nginx-mig --template=$TEMPLATE --zone=$ZONE
-gcloud compute instance-groups managed rolling-action replace nginx-mig --zone=$ZONE
+gcloud compute instance-groups managed set-instance-template nginx-mig \
+  --template=$TEMPLATE \
+  --zone=$ZONE
+
+# Rolling update delle VM
+gcloud compute instance-groups managed rolling-action replace nginx-mig \
+  --zone=$ZONE
 
