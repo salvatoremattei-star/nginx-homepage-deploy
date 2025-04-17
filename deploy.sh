9f67fcade61eca10c4805b68adb92e13ce47f813
root <salvatore.mattei@digimetrica.com>
2025-04-17 12:37:03 +0200

    test15

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ resources:
             #!/bin/bash
             apt-get update
             apt-get install -y nginx
-	    echo '$(cat html/index.html)' > /var/www/html/index.html
+            echo '$(cat html/index.html)' > /var/www/html/index.html
             systemctl enable nginx
             systemctl restart nginx
       networkInterfaces:
