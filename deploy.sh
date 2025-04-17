e214c819a3ac5f451cdfa500ab7a6ec1f388a512
root <salvatore.mattei@digimetrica.com>
2025-04-17 11:57:37 +0200

    test10

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 set -e
 
-# Impostazioni progetto
+# Configurazione progetto
 PROJECT_ID=playground-s-11-0a723cfb
 REGION=us-central1
 ZONE=us-central1-a
 SUBNET=custom-subnet
 
-# Script di avvio (startup-script) da inserire nel template
-STARTUP_SCRIPT=$(cat <<'EOT'
+# Carica il file HTML dal repository e lo inserisce nella startup-script
+STARTUP_SCRIPT=$(cat <<EOT
         value: |
           #!/bin/bash
           apt-get update
           apt-get install -y nginx
-          cat <<EOF_HTML > /var/www/html/index.html
-          <!DOCTYPE html>
-          <html>
-          <head><title>Homepage</title></head>
-          <body>
-            <h1>Benvenuto sulla homepage aggiornata!</h1>
-          </body>
-          </html>
-EOF_HTML
+          cat <<EOF > /var/www/html/index.html
+$(cat html/index.html)
+EOF
           systemctl enable nginx
           systemctl restart nginx
 EOT
 )
 
-# Nome univoco per il nuovo template
+# Crea un nome univoco per l'instance template
 TEMPLATE_NAME=homepage-template-$(date +%s)
 
-# Creazione file YAML del nuovo instance template
+# Crea il file YAML del template
 cat <<EOF > new-template.yaml
 resources:
 - name: $TEMPLATE_NAME
@@ -61,10 +55,10 @@ $STARTUP_SCRIPT
         scopes: [https://www.googleapis.com/auth/cloud-platform]
 EOF
 
-# Deploy del nuovo template
+# Crea il nuovo template
 gcloud deployment-manager deployments create $TEMPLATE_NAME --config=new-template.yaml
 
-# Recupero del selfLink del template appena creato
+# Ottiene il selfLink
 TEMPLATE=$(gcloud compute instance-templates list \
   --filter="name=$TEMPLATE_NAME" \
   --format="value(selfLink)" | head -n 1)
@@ -74,7 +68,7 @@ gcloud compute instance-groups managed set-instance-template nginx-mig \
   --template=$TEMPLATE \
   --zone=$ZONE
 
-# Rolling update delle VM
+# Rolling update
 gcloud compute instance-groups managed rolling-action replace nginx-mig \
   --zone=$ZONE
 
